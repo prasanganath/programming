4c6a129440b99fe1c0942e2d12c8c0ffc44640cd
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-03-11 10:12:05 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class dsa{
+	 public static void main(String[] args) {
+	 	Scanner Scan =new Scanner(System.in); 
+	 		System.out.println("Enter your number");
+	 		int user_input_number=Scan.nextInt();
+	 		System.out.println("The enterd value is" +user_input_number);
+	 public static void main(String[] args){	
+	    Scanner Scan1 =new Scanner(System.in); 
+	 		System.out.println("Enter your number2");
+	 		int user_input_number=Scan1.nextInt();
+	 		System.out.println("The enterd value is" +user_input_number);
+	 	
+	    
+
+	}
+}
+
+}
